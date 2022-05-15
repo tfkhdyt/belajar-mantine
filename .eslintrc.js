b0bd5492b2f484aeb9021552066902aa938a468d
tfkhdyt <tfkhdyt@pm.me>
2022-05-15 19:02:46 +0700

    setup mantine provider

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,19 +4,21 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "next/core-web-vitals",
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
-  rules: {},
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily: 'serif',
+        colorScheme: 'dark',
+        fontSizes: {
+          md: 12,
+        },
+        loader: 'dots',
+        radius: {
+          sm: 15,
+        },
+        colors: {
+          blue: [
+            '#7958f2',
+            '#364fc7',
+            '#99e9f2',
+            '#fd7e14',
+            '#f59f00',
+            '#5c940d',
+            '#12b886',
+            '#4c6ef5',
+            '#ae3ec9',
+            '#f03e3e',
+          ],
+        },
+      }}
+      // styles={{
+      //   Button: (theme) => {
+      //     root: {
+      //       backgroundColor: theme.colors.blue[1];
+      //     }
+      //   },
+      // }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
